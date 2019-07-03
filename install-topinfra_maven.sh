5f8c990e71b17e0972e3d78fe6049f8dc4ec9086
haolun zhang <chshawkn@users.noreply.github.com>
2019-07-04 04:00:25 +0800

    upgrade install-topinfra_maven.sh

--- install-topinfra_maven.sh
+++ install-topinfra_maven.sh
@@ -3,6 +3,7 @@
 if ! type -p brew > /dev/null; then /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; fi
 if [[ -n $(brew list | grep '^maven$') ]]; then brew unlink maven; fi
 if [[ -n $(brew list | grep '^mvnvm$') ]]; then brew unlink mvnvm; fi
+brew update
 # `brew install ci-and-cd/topinfra/topinfra-maven` or `brew reinstall ci-and-cd/topinfra/topinfra-maven` to re-install snapshot versions
 if [[ -z $(brew list | grep '^topinfra-maven$') ]]; then brew install ci-and-cd/topinfra/topinfra-maven; else brew reinstall ci-and-cd/topinfra/topinfra-maven; fi
 
